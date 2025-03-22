2d55e11e715803624e33fa8492bde87a6f8c6bd5
Mailos07 <mailosg@yahoo.com>
2025-03-22 18:59:02 -0400

    added new states

--- backend/TransparenSee/src/main/java/gov/transparenSee/TransparenSee/CustomerVerification.java
+++ backend/TransparenSee/src/main/java/gov/transparenSee/TransparenSee/CustomerVerification.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class CustomerVerification {
 
     public enum Region {
-        NORTH, NORTHWEST, NORTHEAST, SOUTHWEST, SOUTHEAST, SOUTH, EAST, WEST, CENTRAL
+        Alabama,Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, NewHampshire, NewJersey, NewMexico, NewYork, NorthCarolina, NorthDakota, Ohio, Oklahoma, Oregon, Pennsylvania, RhodeIsland, SouthCarolina, SouthDakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, WestVirginia, Wisconsin, Wyoming
     }
 
     public enum FieldOfOperation {
